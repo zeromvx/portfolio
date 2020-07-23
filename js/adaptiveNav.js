7e739a2c0181a9019034d5cd8e6a388d143e2776
zeromvx <zeromvx@gmail.com>
2020-07-23 19:01:01 +1000

    Fixed bugs with headers adaptive

--- js/adaptiveNav.js
+++ js/adaptiveNav.js
@@ -1,25 +1,40 @@
-const toggleBtn = document.querySelector(".toggle-icon"); // toggle button
+const toggleBtn = document.querySelector(".toggle-btn"); // toggle button
 const icon = document.querySelector(".toggle-icon");
 const header = document.querySelector(".header"); // header section
 const mainContent = document.querySelector(".main-content"); // main content section
+const navList = document.querySelector('.nav'); 
+
+function addActiveClasses() {
+	toggleBtn.classList.add("active");
+	icon.classList.add("open");
+	header.classList.add("header--active");
+}
+
+function removeActiveClasses() {
+	toggleBtn.classList.remove("active");
+	icon.classList.remove("open");
+	header.classList.remove("header--active");
+}
 
 toggleBtn.addEventListener("click", function () {
 	if (!toggleBtn.classList.contains("active")) {
-		toggleBtn.classList.add("active");
-		icon.classList.add("open");
-		header.style.left = "0";
+		addActiveClasses();
 	} else {
-		toggleBtn.classList.remove("active");
-		icon.classList.remove("open");
-		header.style.left = "-100%";
+		removeActiveClasses();
 	}
 });
 
+
+// removing header on click outside the header
 mainContent.addEventListener("click", function () {
 	if (toggleBtn.classList.contains("active")) {
-		toggleBtn.classList.remove("active");
-		icon.classList.remove("open");
-		header.style.left = "-100%";
+		removeActiveClasses();
 	}
 });
 
+// removing header on nav links click
+navList.addEventListener('click', (e) => {
+	if (e.target.tagName === "A"){
+		removeActiveClasses();
+	}
+});
